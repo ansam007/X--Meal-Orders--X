49aebbb15169ec0ed1ca703db932124b611d627b
ansam007 <anshul.sharma5271@gmail.com>
2024-02-16 18:23:17 +0530

    made a card to wrap my meal summary

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import Header from "./components/Header/Header";
+import MealSummary from "./components/Meals/MealSummary";
+import React from "react";
 
 function App() {
   return (
-  <Header></Header>
+    <React.Fragment>
+      <Header></Header>
+      <MealSummary></MealSummary>
+    </React.Fragment>
   );
 }
 
-export default App; 
+export default App;
---
+++ src/components/Meals/MealSummary.js
@@ -0,0 +1,21 @@
+import Card from "../UI/Card";
+
+const MealSummary = () => {
+  return (
+    <Card>
+      <section>
+        <h1> Delicious Food, Delivered To you</h1>
+        <p>
+          Choose your favourite meal from our broad selection of available meals
+          and enjoy a delicious lunch or dinner at home
+        </p>
+        <p>
+          All our meals are cooked with high-quality ingredients, just-in-time
+          and of course by experienced chefs!
+        </p>
+      </section>
+    </Card>
+  );
+};
+
+export default MealSummary;
