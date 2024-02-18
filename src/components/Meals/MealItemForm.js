c7aa7b773e0f6982b7e654a57b41bb40d7751037
ansam007 <anshul.sharma5271@gmail.com>
2024-02-18 12:30:03 +0530

    add styling with the help of resources to make it look good

--- src/components/Meals/MealItemForm.js
+++ src/components/Meals/MealItemForm.js
@@ -1,13 +1,23 @@
-import classes from './MealItemForm.module.css';
+import classes from "./MealItemForm.module.css";
+import Input from "../UI/Input";
 
 const MealItemForm = () => {
-    return (
-        <form className={classes.form}>
-            <label htmlFor="amount">Amount</label>
-            <input id='amount' type='number' min='1' max='5' step='1' defaultValue='1'></input>
-            <button>+ Add</button>
-        </form>
-    )
-}
+  return (
+    <form className={classes.form}>
+      <Input
+        label="Amount"
+        input={{
+          id: "amount",
+          type: "number",
+          min: "1",
+          max: "5",
+          step: "1",
+          defaultValue: "1",
+        }}
+      ></Input>
+      <button>+ Add</button>
+    </form>
+  );
+};
 
 export default MealItemForm;
